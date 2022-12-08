9ac8bb3aaf40f081db8e682738251d15aaeb30de
Nathan <xxlunix@yahoo.com>
2022-12-07 16:49:05 -0800

    db2

--- Database/node/node.js
+++ Database/node/node.js
@@ -1,1 +1,24 @@
-find
+const { MongoClient } = require("mongodb");
+// Replace the uri string with your MongoDB deployment's connection string.
+const uri =
+  "mongodb+srv://tester123:12345@cluster0.mzsfais.mongodb.net/test";
+const client = new MongoClient(uri);
+async function run() {
+  try {
+    await client.connect();
+    // database and collection code goes here
+    const db = client.db("coinsDB");
+    const coll = db.collection("CoinsCollection");
+
+    // find code goes here
+    const cursor = coll.find();
+
+    // iterate code goes here
+    await cursor.forEach(console.log);
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
